950dd76d0cd6cc52832af948e9863b7fc0891c05
Андрей Вшивков <andreanpal@gmail.com>
2017-08-11 01:50:50 +0500

    Change area detection

---
+++ config.py
@@ -0,0 +1,38 @@
+
+
+Time_Width = 0.12
+Time_Height = 0.047
+Time_CenterX = 0.19 # * height + width/2
+Time_CenterY = 0.611
+Time_WX0 = 0.140 
+Time_WX1 = 0.239
+Time_Y0 = 0.592
+Time_Y1 = 0.630
+
+TIME_AREA = (0.592, 0.630, 0.14, 0.239)
+	
+
+Cp_Width = 0.32
+Cp_Height = 0.068
+Cp_X0 = 0.1
+Cp_X1 = 0.7
+Cp_Y0 = 0.163
+Cp_Y1 = 0.231
+
+CP_AREA = (0.163, 0.238, 0, 1)
+
+Poke_Height = 0.075
+Poke_X0 = 0.2
+Poke_X1 = 0.8
+Poke_Y0 = 0.245
+Poke_Y1 = 0.325
+
+POKE_AREA = (0.244, 0.325, 0, 1)
+
+Gym_X0 = 0.105
+Gym_X1 = 0.9
+Gym_Y0 = 0.046
+Gym_Y1 = 0.128
+
+GYM_AREA = (0.046, 0.126, 0.105, 0.9)
+
--- parse_raid.py
+++ parse_raid.py
@@ -1,9 +1,54 @@
 #!/usr/bin/python3
 import cv2
 import numpy as np
+import config
 from sys import argv
 from PIL import Image
 from tesserocr import PyTessBaseAPI
+
+
+# input has relative values
+class Area(object):
+    def __init__(self, coords, img, relative=False, relativeCenterX=False):
+        self.img = img
+        rows, cols, ch = img.shape
+
+        if relative:
+            coords = list(map(lambda x: int(x*rows), coords))
+            if relativeCenterX:
+                coords[2] += int(cols/2)		
+                coords[3] += int(cols/2)
+        self.y0 = coords[0]
+        self.y1 = coords[1]
+        self.x0 = coords[2]
+        self.x1 = coords[3]		
+        self.roi = img[self.y0:self.y1, self.x0:self.x1]
+
+
+class FilteredArea(Area):
+    def __init__(self, coords, color_low, color_high, hsv, relative=False, relativeCenterX=False):
+        super().__init__(coords, hsv, relative, relativeCenterX)
+        self.low = np.array(color_low)
+        self.high = np.array(color_high)
+	
+    def getMask(self):		
+        mask = cv2.inRange(self.roi, self.low , self.high);
+        # mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((3,3), np.uint8))
+        return mask
+
+
+def removeBlackArea(hsv):
+    rows, cols, _ = hsv.shape
+    tested_area = (int(rows - rows*0.1), rows, 0, cols)
+    black_area = FilteredArea(tested_area, [0,0,0], [10, 255, 5], hsv)
+    vector_avg = cv2.reduce(black_area.getMask(), 1, cv2.REDUCE_AVG)
+    v_rows, _ = vector_avg.shape
+    i = v_rows - 1
+    while i > 0 and vector_avg[i, 0] > 5:
+        i -= 1
+    black_area_height = v_rows - 1 - i	
+    return hsv[0:rows-black_area_height, 0:cols]
+
 
 def getWhiteArea(hsv, low, high):
     mask = cv2.inRange(hsv, low, high)
@@ -21,41 +66,45 @@ def main():
 
     print("Read image")
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    gym_area  = hsv[70:140, 140:700]
-    time_area = hsv[767:800, 540:670]
-    cp_area   = hsv[205:310, 217:580]
-    poke_area = hsv[300:400, 20:700]        
+    hsv = removeBlackArea(hsv)
 
-    gym_m = getWhiteArea(gym_area,   np.array([0,0,210]), np.array([180, 80, 255]))
-    time_m = getWhiteArea(time_area, np.array([0,0,230]), np.array([180, 60, 255]))
-    cp_m = getWhiteArea(cp_area,     np.array([0,0,252]), np.array([180, 20, 255]))
-    poke_m = getWhiteArea(poke_area, np.array([0,0,250]), np.array([180, 25, 255]))
-
-    # convert to Pillow format
-#    gym_m = cv2.cvtColor(gym_m, cv2.COLOR_GRAY2RGB)
-    gym_pm = Image.fromarray(gym_m)
-
-#    time_m = cv2.cvtColor(time_m, cv2.COLOR_BGR2RGB)
-    time_pm = Image.fromarray(time_m)
-
-#    cp_m = cv2.cvtColor(cp_m, cv2.COLOR_BGR2RGB)    
-    cp_pm = Image.fromarray(cp_m)
+    gym_area  = FilteredArea(config.GYM_AREA,  [0, 0, 210], [180, 80, 255], hsv, relative=True)		
+    time_area = FilteredArea(config.TIME_AREA, [0, 0, 230], [180, 60, 255], hsv, relative=True, relativeCenterX=True)
+    cp_area   = FilteredArea(config.CP_AREA,   [0, 0, 252], [180, 20, 255], hsv, relative=True)
+    poke_area = FilteredArea(config.POKE_AREA, [0, 0, 250], [180, 25, 255], hsv, relative=True)		
     
-#    poke_m = cv2.cvtColor(poke_m, cv2.COLOR_BGR2RGB)
-    poke_pm = Image.fromarray(poke_m)
+    areas = [gym_area, time_area, cp_area, poke_area]
+    
+    gym_pm, time_pm, cp_pm, poke_pm = [Image.fromarray(x.getMask()) for x in areas]
 
+## convert to Pillow format
+##    gym_m = cv2.cvtColor(gym_m, cv2.COLOR_GRAY2RGB)
+#    gym_pm = Image.fromarray(gym_m)
+#
+##    time_m = cv2.cvtColor(time_m, cv2.COLOR_BGR2RGB)
+#    time_pm = Image.fromarray(time_m)
+#
+##    cp_m = cv2.cvtColor(cp_m, cv2.COLOR_BGR2RGB)    
+#    cp_pm = Image.fromarray(cp_m)
+#    
+##    poke_m = cv2.cvtColor(poke_m, cv2.COLOR_BGR2RGB)
+#    poke_pm = Image.fromarray(poke_m)
+#
     gym_text_eng = ""
 
     with PyTessBaseAPI() as api:
         
         api.SetImage(gym_pm)
-        gym_text_eng = api.GetUTF8Text()
-        
+        gym_text_eng = api.GetUTF8Text()        
+        print("Gym ENG: ", gym_text_eng)
+        print(api.AllWordConfidences())
+
         # for time
-        api.SetVariable('tessedit_char_whitelist', '0123456789:')
+        api.SetVariable('tessedit_char_whitelist', 'cp0123456789:')
         api.SetImage(time_pm)
     
         print("Raid Time: ", api.GetUTF8Text())
+        print(api.AllWordConfidences())
     
         api.SetVariable('tessedit_char_whitelist', '0123456789')
         api.SetImage(cp_pm)
@@ -64,12 +113,13 @@ def main():
         api.SetVariable('tessedit_char_whitelist', 'abcdefghijklmnopqrstuvwxyzBCDEFGHIJKLMNOPQRSTUVWXYZ')
         api.SetImage(poke_pm)
         print("Poke Name: ", api.GetUTF8Text())
+        print(api.AllWordConfidences())
 
 
     with PyTessBaseAPI(lang='rus') as api:
         api.SetImage(gym_pm)
         print("Gym RUS: ", api.GetUTF8Text())
-        print("Gym ENG: ", gym_text_eng)
+        print(api.AllWordConfidences())
 
 main()
 
